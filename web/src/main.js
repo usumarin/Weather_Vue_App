83acf1d1151cf65c275862cd4995345e456cf6a0
usumarin <ubuntu2682ch@gmail.com>
2020-12-06 09:53:38 +0900

    :gear: add vue-awesome

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "vue-awesome/icons";
+import Icon from "vue-awesome/components/Icon";
 
 Vue.config.productionTip = false;
+Vue.component("v-icon", Icon);
 
 new Vue({
   render: (h) => h(App),
